a88d210001985a6e446dce910c1401cac68521ce
ifyr <cyifyr@gmail.com>
2015-04-29 18:00:55 +0800

    Version 1.0

---
+++ src/pw/ifyr/CharKeyboardView.java
@@ -0,0 +1,16 @@
+package pw.ifyr;
+
+import android.content.Context;
+import android.inputmethodservice.KeyboardView;
+import android.util.AttributeSet;
+
+public class CharKeyboardView extends KeyboardView {
+
+	public CharKeyboardView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CharKeyboardView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+}
